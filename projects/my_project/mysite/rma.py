d9017119831164c3f90ad24d0a1704030ece3575
DanielShaddix <kendall9danil7kds@gmail.com>
2019-03-07 18:33:25 +0200

    first commit

--- projects/my_project/mysite/rma.py
+++ projects/my_project/mysite/rma.py
@@ -1,1 +1,30 @@
-rma_team = {'Bale':70, 'Benzema': 40, 'Modric': 25, 'Ramos': 30, 'Kroos': 75}
+[33mcommit c9b633ac91cf16a64517bc65d04045ff08e0d917[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: DanielShaddix <kendall9danil7kds@gmail.com>
+Date:   Thu Mar 7 18:22:46 2019 +0200
+
+    first commit
+
+[33mcommit c403feab675c6c56251ee83ae5f2b76140b822f9[m
+Merge: 2fb6a78 df50825
+Author: DanielShaddix <kendall9danil7kds@gmail.com>
+Date:   Thu Mar 7 17:09:42 2019 +0200
+
+    first commit
+
+[33mcommit 2fb6a78893de1f16cd65296d28ebf8381f418251[m
+Author: DanielShaddix <kendall9danil7kds@gmail.com>
+Date:   Thu Mar 7 16:44:25 2019 +0200
+
+    first commit
+
+[33mcommit 7a5d00d6ce9ab2850a10b420da9a76b7bf8729c4[m
+Author: DanielShaddix <kendall9danil7kds@gmail.com>
+Date:   Tue Mar 5 14:49:04 2019 +0200
+
+    release v1
+
+[33mcommit df508251607c9acc40a8dd74c6020b0d7c19f98d[m
+Author: DanielShaddix <48245347+DanielShaddix@users.noreply.github.com>
+Date:   Tue Mar 5 14:00:26 2019 +0200
+
+    Initial commit
