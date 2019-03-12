4ecea9393aa76faf35e35184a6f796091b5d9022
DanielShaddix <kendall9danil7kds@gmail.com>
2019-03-12 14:04:14 +0200

    first commit

--- projects/my_project/mysite/try/rma.py
+++ projects/my_project/mysite/try/rma.py
@@ -1,2 +1,2 @@
-rma_team = {'Bale':70, 'Benzema': 40, 'Modric': 25, 'Ramos': 30, 'Kroos': 75, 'Navas': 15, 'Cortua': 35}
+rma_team = {'Bale':70, 'Benzema': 40, 'Modric': 25, 'Ramos': 30, 'Kroos': 75, 'Navas': 15, 'Cortua': 35, 'Casemiro':30}
 
